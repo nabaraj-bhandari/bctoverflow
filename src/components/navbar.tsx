85e39351d651cadfabd30679701cb1ca802bd74c
Nabaraj Bhandari <nabarajbhandari2005@gmail.com>
2026-01-24 21:46:17 +0545

    added logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
-import { LaptopMinimal } from "lucide-react";
+
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="w-full sticky top-0 z-50 bg-background/80 backdrop-blur-xl border-b border-border px-4 py-3">
-      <Link href="/" className="flex justify-center items-center select-none">
-        <LaptopMinimal className="mr-2 text-accent" />
-        <span className="font-bold text-lg ">BCT Overflow</span>
+      <Link href="/" className="flex justify-center items-center select-none gap-2">
+        <Image src={'/logo2.png'} alt="BCT Overflow" width={32} height={32} className="rounded-md" />
+        <span className="font-bold text-lg">BCT Overflow</span>
       </Link>
     </nav>
   );
